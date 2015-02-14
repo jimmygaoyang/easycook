80e066213768459b478abe4f134113319a06b481
jimmygaoyang <jimmygaoyang@163.com>
2015-02-14 22:46:24 +0800

    首次创建易厨站

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"></meta>
+	<title><?php echo $title?>ECOOK</title>
+	<link rel="stylesheet" type="text/css" href="/htdocs/bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="/htdocs/js/jquery-2.1.3.min.js"></script>
+	<script src="/htdocs/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
