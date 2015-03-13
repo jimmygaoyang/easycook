0989254d04d580d171cd622f16b361de74ce1299
jimmygaoyang <jimmygaoyang@163.com>
2015-03-13 13:25:10 +0800

    加入所有的模型

---
+++ application/models/role.php
@@ -0,0 +1,12 @@
+
+<?php
+class Material extends MY_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->table_name = "Material";
+		$this->fields = $this->db->list_fields($this->table_name);
+		$this->primary_key = "Material";
+	}
+}
